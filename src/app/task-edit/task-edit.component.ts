149c7d9537918109b4c146725f5de6236ea69abc
Takafumi Kamada <tkamada696@gmail.com>
2022-04-09 21:10:17 +0900

    Change: 追加時のアフォーダンスを追加

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { TaskManagerService } from '../task-manager.service';
 
@@ -8,12 +9,19 @@ import { TaskManagerService } from '../task-manager.service';
   styleUrls: ['./task-edit.component.scss'],
 })
 export class TaskEditComponent implements OnInit {
-  constructor(public taskManager: TaskManagerService, private router: Router) {}
+  constructor(
+    public taskManager: TaskManagerService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
 
   addTask(taskName: string) {
     this.taskManager.addTask(taskName);
+    this.snackBar.open('追加しました！', undefined, {
+      duration: 2000,
+    });
     this.router.navigate(['']);
   }
 }
