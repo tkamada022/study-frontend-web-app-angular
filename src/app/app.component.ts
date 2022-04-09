8e4491c7ffd71f3787ba981bdbc24be1f574fd23
Takafumi Kamada <tkamada696@gmail.com>
2022-04-09 15:16:47 +0900

    Change: タスクの管理機能を追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  tasks = [
+    {
+      name: 'ライブの申込み',
+      isCompleted: true,
+    },
+    {
+      name: 'セトリの予想',
+      isCompleted: true,
+    },
+  ];
+
+  addTask(taskName: string) {
+    this.tasks.push({
+      name: taskName,
+      isCompleted: false,
+    });
+  }
+
+  getNumOfCompleteTasks() {
+    let completedTaskNum = 0;
+
+    for (let task of this.tasks) {
+      if (task.isCompleted == true) {
+        completedTaskNum++;
+      }
+    }
+
+    return completedTaskNum;
+  }
 }
