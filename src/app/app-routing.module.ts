3763eb5ec9a8eaaaba0dcc982f0cbeb1992c0f87
Takafumi Kamada <tkamada696@gmail.com>
2022-04-16 11:53:04 +0900

    Update: 保存するイベントをselectionChangeを使用するように変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TaskEditComponent } from './task-edit/task-edit.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
 
 const routes: Routes = [
   {
